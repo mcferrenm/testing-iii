809853b8d21de1bad9c89c33cc5cb673f23a7c1f
mcferrenm <mcferrenmax@gmail.com>
2019-03-06 16:30:58 -0500

    Test Display to match snapshot

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from "react";
+import renderer from "react-test-renderer"
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Display />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
