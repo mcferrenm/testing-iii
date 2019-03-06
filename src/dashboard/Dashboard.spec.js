32bd177e14619884bfb3735a8d55d209ce178e44
mcferrenm <mcferrenmax@gmail.com>
2019-03-06 16:14:50 -0500

    Test Dashboard to render display and controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Dashboard from './Dashboard';
+import React from "react";
+import renderer from "react-test-renderer";
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
 
-describe('matches snapshot', () => {
-  it('matches snapshot', () => {
+import Dashboard from "./Dashboard";
+
+describe("matches snapshot", () => {
+  it("matches snapshot", () => {
     const tree = renderer.create(<Dashboard />);
 
     expect(tree.toJSON()).toMatchSnapshot();
-  })
+  });
+
+  it("should render display and controls", () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    const displayComponent = getByTestId(/display panel/i);
+    const controlsComponent = getByTestId(/controls panel/i);
+
+    expect(displayComponent).toBeInTheDocument();
+    expect(controlsComponent).toBeInTheDocument();
+  });
 });
