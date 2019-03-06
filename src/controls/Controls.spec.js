ec9faec85e5799feaab69c3975a8c050a391a58c
mcferrenm <mcferrenmax@gmail.com>
2019-03-06 16:17:28 -0500

    Test Controls to match snapshot

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
