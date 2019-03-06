32652b395c2e437a1ed1260484564c50aefc20d3
mcferrenm <mcferrenmax@gmail.com>
2019-03-06 15:59:36 -0500

    Test Dashboard to match snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Dashboard from './Dashboard';
+
+describe('matches snapshot', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  })
+});
